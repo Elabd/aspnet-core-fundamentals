32cb1da8c9e75efccee4c23495f5090584a391f0
Elabd <aabeltwab@fcih1.com>
2019-10-22 13:22:39 +0200

    Add project files.

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,58 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+
+        IEnumerable<Restaurant> GetRestaurantsByName(string name = null);
+        Restaurant GetRestaurantById(int id);
+        Restaurant UpdateRestaurant(Restaurant updatedRestaurant);
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        readonly List<Restaurant> _restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            _restaurants = new List<Restaurant>()
+            {
+                new Restaurant { Id = 1, Name = "Scott's Pizza", Location="Maryland", Cuisine=CuisineType.Italian},
+                new Restaurant { Id = 2, Name = "Cinnamon Club", Location="London", Cuisine=CuisineType.Italian},
+                new Restaurant { Id = 3, Name = "La Costa", Location = "California", Cuisine=CuisineType.Mexican}
+            };
+        }
+
+
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
+        {
+
+            return from r in _restaurants
+                   where String.IsNullOrEmpty(name) || r.Name.ToLower().StartsWith(name.ToLower())
+                   orderby r.Name
+                   select r;
+        }
+
+        public Restaurant GetRestaurantById(int id)
+        {
+            return _restaurants.SingleOrDefault(x => x.Id == id);
+        }
+
+        public Restaurant UpdateRestaurant(Restaurant updatedRestaurant)
+        {
+            var restaurant = _restaurants.Single(r => r.Id == updatedRestaurant.Id);
+            restaurant.Name = updatedRestaurant.Name;
+            restaurant.Cuisine = updatedRestaurant.Cuisine;
+            restaurant.Location = updatedRestaurant.Location;
+
+            return restaurant;
+        }
+
+    }
+}
---
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OdeToFood.Core;
+using OdeToFood.Data;
+using System.Collections.Generic;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class EditModel : PageModel
+    {
+
+        private readonly IRestaurantData _restaurantData;
+        private readonly IHtmlHelper _htmlHelper;
+        public Restaurant Restaurant { get; set; }
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
+        public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelper)
+        {
+            _restaurantData = restaurantData;
+            _htmlHelper = htmlHelper;
+        }
+        public IActionResult OnGet(int restaurantId)
+        {
+            Cuisines = _htmlHelper.GetEnumSelectList<CuisineType>();
+            Restaurant = _restaurantData.GetRestaurantById(restaurantId);
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            return Page();
+        }
+        public IActionResult OnPost(Restaurant restaurant)
+        {
+            Cuisines = _htmlHelper.GetEnumSelectList<CuisineType>();
+            Restaurant = _restaurantData.UpdateRestaurant(restaurant);
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+            return Page();
+        }
+    }
+}
