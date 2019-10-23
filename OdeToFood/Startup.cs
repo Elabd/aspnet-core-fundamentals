47d83aca509db3350bfed1ab86f8e528ab199e9b
Elabd <aabeltwab@fcih1.com>
2019-10-23 17:16:27 +0200

    Add Node Module and datatable

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -54,6 +54,7 @@ namespace OdeToFood
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseNodeModules(env);
             app.UseCookiePolicy();
 
             app.UseMvc();
