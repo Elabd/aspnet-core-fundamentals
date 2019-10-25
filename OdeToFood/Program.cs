f11af91060883dbd116e9368d6ae23ddb9f3f33d
Elabd <aabeltwab@fcih1.com>
2019-10-26 01:16:35 +0200

    Production prepear

--- OdeToFood/Program.cs
+++ OdeToFood/Program.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OdeToFood.Data;
+using System;
 
 namespace OdeToFood
 {
@@ -7,8 +11,40 @@ namespace OdeToFood
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+
+            var host = CreateWebHostBuilder(args).Build();
+            CreateDbIfNotExists(host);
+            host.Run();
         }
+
+        //private static void MigrateDatabase(IWebHost host)
+        //{
+        //    using (var scope = host.Services.CreateScope())
+        //    {
+        //        var db = scope.ServiceProvider.GetRequiredService<OdeToFoodDbContext>();
+        //        db.Database.Migrate();
+        //    }
+        //}
+
+        private static void CreateDbIfNotExists(IWebHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    var context = services.GetRequiredService<OdeToFoodDbContext>();
+                    context.Database.EnsureCreated();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+        }
+
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
